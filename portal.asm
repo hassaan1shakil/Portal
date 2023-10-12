3eb18501cc4c0f556170e818131f91eccbfea624
hassaan1shakil <112756561+hassaan1shakil@users.noreply.github.com>
2023-10-12 19:46:14 +0500

    Add files via upload

--- portal.asm
+++ portal.asm
@@ -484,7 +484,7 @@ ClearScreen:
     mov dx, 0;		                    ; starting row
     
     mov ah, 0x0C;
-    mov al, 26		                    ; color
+    mov al, 24		                    ; color
 
     mov cx, 0
     mov dx, 66                          ; only printing 2nd section
@@ -499,6 +499,10 @@ ClearScreen:
             jnz clear_rows;
     
         add dx, 1;
+
+		cmp dx, 74
+		je change_ceiling_color
+
         cmp dx, 135                     ; limited to 2nd section only             
         jnz clear_columns;
 	
@@ -507,6 +511,10 @@ ClearScreen:
     pop ax
 	
 	ret
+
+	change_ceiling_color:
+		mov al, 27
+		jmp clear_columns
 
 PrintConveyorBelt:
     push bp
@@ -541,16 +549,16 @@ PrintConveyorBelt:
 		jnz conveyor_columns;
 
 	conveyor_strip:
-		mov cx, word [bp + 14];		        ; starting col
-		mov dx, [bp + 12];		            ; starting row
+		mov cx, word [bp + 14];		    ; starting col
+		mov dx, [bp + 12];		        ; starting row
 		add dx, 3
 		
 		mov ah, 0x0C;
-		mov al, 24					        ; color
-		mov bx, [cols]                   	; height
+		mov al, 24					    ; color
+		mov bx, [cols]                	; height
 
 		strip_columns:
-			mov cx, 0		                ; width
+			mov cx, 0		            ; width
 
 			strip_rows:
 				int 10h;
@@ -564,7 +572,7 @@ PrintConveyorBelt:
 
     xor bx, bx
 		mov ah, 0x0C;
-		mov al, 0x12				        ; color
+		mov al, 0x12				    ; color
 
     BeltSupports:                       ; dx is already on the next row here
         
@@ -593,10 +601,6 @@ PrintConveyorBelt:
     pop bx
     pop ax 
     pop bp 
-
-	;interrupt for key press ;; is this needed here???
-	mov ah, 00h;
-	int 16h;
 	
 	ret 12
 
@@ -646,21 +650,21 @@ PrintBox:
 box_stars:
 	mov cx, 5
 
-	mov dx, 0D03h  ; Row=12 (in DH), Column=20 (in DL)
-	mov bh, 0      ; Page=0
-	mov ah, 02h    ; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
+	mov dx, 0D03h  					; Row=12 (in DH), Column=20 (in DL)
+	mov bh, 0      					; Page=0
+	mov ah, 02h    					; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
 	int 10h
 
-	mov bx, 000Eh  ; Page=0 (in BH), Color=14 (in BL)             14 is Yellow
-	mov ax, 0E2Ah  ; BIOS.Teletype (in AH), Character=33 (in AL)  33 is ASCII of "!"
+	mov bx, 000Eh  					; Page=0 (in BH), Color=14 (in BL)             14 is Yellow
+	mov ax, 0E2Ah  					; BIOS.Teletype (in AH), Character=33 (in AL)  33 is ASCII of "!"
 
 	loop_decor:
 
-		mov ah, 02h    ; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
+		mov ah, 02h    				; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
 		int 10h
 
-		mov ax, 0E2Ah  ; BIOS.Teletype (in AH), Character=33 (in AL)  33 is ASCII of "!"
-		int 10h		; print interrupt
+		mov ax, 0E2Ah  				; BIOS.Teletype (in AH), Character=33 (in AL)  33 is ASCII of "!"
+		int 10h						; print interrupt
 
 		sub cx, 1
 		cmp cx, 2
@@ -687,6 +691,91 @@ set_color:
 	jmp box_columns
 
 
+PrintCeiling:
+	push bp
+	mov bp, sp
+
+    push ax
+    push bx
+    push cx
+    push dx
+
+	mov ax, 0A000h;
+    mov es, ax;
+    mov cx, 36						; starting column
+    mov dx, 66		        		; starting row
+    
+    mov ah, 0x0C;
+    mov al, 237					    ; color
+    mov bl, 38              		; width (picking in half register???)
+    mov bh, 4                       ; no. of hooks
+    
+			
+	ceiling_columns:
+		mov cx, 36
+        mov bh, 4                   ; no. of boxes
+
+        ceiling_rows:
+            int 10h;
+            add cx, 1;
+            dec bl
+            jnz ceiling_rows;
+
+        add cx, 30
+        mov bl, 38           		; width
+        dec bh
+        jnz ceiling_rows
+
+        add dx, 1
+        cmp dx, 72                	; hardcoded for now??? formula can be implemented tho???
+        jnz ceiling_columns
+
+
+	ceiling_hooks:					; I presum that the hook end at Row 80 (for shifting purposes)???
+		mov cx, 4
+
+		mov dx, 0906h  				; Row=12 (in DH), Column=20 (in DL)
+		mov bh, 0     				; Page=0
+		mov ah, 02h    				; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
+		int 10h
+
+		mov bx, 0006h  				; Page=0 (in BH), Color=14 (in BL)             14 is Yellow
+		mov ax, 0E7Ch  				; BIOS.Teletype (in AH), Character=7C
+
+		loop_hooks:
+
+			mov ah, 02h    			; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
+			int 10h
+
+			mov ax, 0E7Ch  			; BIOS.Teletype (in AH), Character=7C
+			int 10h					; print interrupt
+
+			sub cx, 1
+			cmp cx, 2
+			je	change_gap
+
+			add dl, 9;
+			cmp cx, 0
+			jnz loop_hooks
+
+	pop dx
+    pop cx
+    pop bx
+    pop ax 
+    pop bp 
+
+	;interrupt for key press ;; is this needed here???
+	mov ah, 00h;
+	int 16h;
+
+	ret
+
+	change_gap:
+		add dx, 8
+		cmp cx, 0
+		jnz loop_hooks
+
+
 PrintMiddle:
 
     call ClearScreen
@@ -706,6 +795,8 @@ PrintMiddle:
 	push 0x0073			            ; starting row.....[bp + 12]
 
     call PrintConveyorBelt
+
+	call PrintCeiling
 
     ret
 
