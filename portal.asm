2d663f22da0af75129d5088c945b78e059779294
hassaan1shakil <112756561+hassaan1shakil@users.noreply.github.com>
2023-10-12 12:46:52 +0500

    Add files via upload

--- portal.asm
+++ portal.asm
@@ -105,8 +105,8 @@ printStar:
 		
 ;--------------------------------------------------------------------
 ; Prints a Asteroid at any given locatoin
-; Parameters: center y [bp + 16], center x [bp + 14]
-; Clobbers:   AX, BX, CX, DX, ES, BP
+; Parameters: center y [bp + 18], center x [bp + 16]
+; Clobbers:   AX, BX, CX, DX, ES, BP, DI
 ; Returns:    <none>
 ;--------------------------------------------------------------------
 printAsteroid:
@@ -116,127 +116,254 @@ printAsteroid:
 		push cx
 		push dx
 		push bx
+		push di
 		
+	; bh: column counter
+	; cx: column
+	; dx: row
+	; di: row counter
+		mov bp, sp
 		mov ax, 0A000h
 		mov es, ax
+		mov cx, [bp + 16]		; starting column
+		mov dx, [bp + 18]		; starting row
+		
+		push 6					; darkes shade of flame
+		push 43					; lighter shade of flame
+		push 23					; darker shade of color
+		push 7					; lighter shade of color
 		mov bp, sp
-		mov cx, [bp + 14]		    ; centre column
-		mov dx, [bp + 16]		    ; centre row
 		
-		mov bh, byte [astSize]
-		sub bh, 4				    ; size - 4
-		; shl bh, 1				    ; size / 2
-		; sub dl, bh				; row - (size / 2)
-		mov bl, 2;
-		
-		mov ah, 0x0C
-		mov al, 23				    ; 23 = dull grey
-		
-	leftIncVert:				    ; loop to draw top left vertex of ast
-		int 10h
-		inc dx
-		dec cx
-		dec bl
-		jnz leftIncVert
-		
-	drawBinary:
-			cmp bh, 0
-			je leftDecVert
-			push 0xCCCC
-			push 2
-			call randGen
-			
-		pop ax;
-		cmp ax, 1;
-		je leftIncreaseVert
-		jne leftDecreaseVert
-		
-	leftDecreaseVert:
-			mov bl, byte [astSize]
-			sub bl, 4
-			shr bl, 1
-			sub bh, bl
-			mov ah, 0x0C
-			mov al, 23		
-		
-		aL2:
-				int 10h
-				inc cx
-				inc dx
-				dec bl
-				jnz aL2
-				jmp drawBinary
-		
-	leftIncreaseVert:
-			mov bl, byte [astSize]
-			sub bl, 4
-			shr bl, 1
-			sub bh, bl
-			mov ah, 0x0C
-			mov al, 23				; 23 = dull grey
-		
-		aL1:
-				int 10h
-				dec cx
-				inc dx
-				dec bl
-				jnz aL1
-				jmp drawBinary
-		
-		mov bl, 2
-	leftDecVert:
-			int 10h
-			inc dx
-			inc cx
-			dec bl
-			jnz leftDecVert
-			
-		mov bl, 2
-	rightIncVert:
-			int 10h
-			dec dx
-			inc cx
-			dec bl
-			jnz rightIncVert
-			
-	rightIncreaseVert:
-			mov bl, byte [astSize]
-			sub bl, 4
-			shr bl, 1
-			mov bl, bh
-			mov ah, 0x0C
-			mov al, 23				; 23 = dull grey
-		
-		aL3:
-				int 10h
-				inc cx
-				dec dx
-				dec bl
-				jnz aL3
-	
-	rightDecreaseVert:
-			mov bl, byte [astSize]
-			sub bl, 4
-			shr bl, 1
-			mov ah, 0x0C
-			mov al, 23		
-		
-		aL4:
-				int 10h
-				dec cx
-				dec dx
-				dec bl
-				jnz aL4
-				
-		mov bl, 2
-	rightDecVert:
-			int 10h
-			dec dx
+; printing first and last 4 rows with increasing/decreasing columns and the centre 7 rows with same columns each
+; to give us a square with round corners
+
+		mov ah, 0x0C;
+		mov al, byte [bp + 2]				; dark colo - 78 for light bluish, 152 for dark shade
+		mov di, 4
+		mov bl, 6
+		add cl, bl
+		inc cl
+
+	initRows:					
+			sub cl, bl						; resetting cl to inital pos after traversing
 			dec cx
-			dec bl
-			jnz rightDecVert
+			add bl, 2						; adding 2 more col limit
+			mov bh, bl
+			push bx							; we will be using bl as counter for shading so we preserve bx as a whole
+			sub bl, 4
+		
+	currInitCol:
+			cmp bh, bl
+			jne carryOn1					; if 'bl' pixels are printed then switch to lighter color
+			mov al, byte [bp]			; lighter color
 			
+		carryOn1:
+			int 10h
+			inc cx
+			dec bh
+			jnz currInitCol
+		
+		pop bx								; getting our bx back
+		
+		flameInitRows:						;printinh flames where our column ends for asteroid
+			shr bh, 1
+			mov al, byte [bp + 4]
+			
+			flameInitCols:
+				int 10h
+				inc cx
+				dec bh
+				jnz flameInitCols
+			
+			push bx
+			mov al, byte [bp + 6]
+			mov bx, 0x0202
+			flameDarkInitCols:
+				int 10h
+				inc cx
+				dec bh
+				jnz flameDarkInitCols
+			sub cl, bl
+			pop bx
+			
+			mov bh, bl
+			shr bh, 1
+			sub cl, bh
+		
+		mov al, byte [bp + 2]	
+		inc dx
+		dec di;
+		jnz initRows
+
+		mov di, 4
+		push dx
+		push bx
+		push cx
+			
+			shr bh, 1
+			add cl, bh
+			; shr di, 1
+			mov al, byte [bp + 6]
+			mov bx, 0x0505
+			add cl, bl
+			
+			flameDarkCenterRows:
+				sub cl, bl
+				add	bl, 3
+				mov bh, bl
+			flameDarkCenterCols:
+				int 10h
+				inc cx
+				dec bh
+				jnz flameDarkCenterCols
+			
+			inc dx
+			dec di
+			jnz flameDarkCenterRows
+		
+		mov di, 4
+			flameDarkCenterRows2:
+				sub cl, bl
+				sub	bl, 3
+				mov bh, bl
+			flameDarkCenterCols2:
+				int 10h
+				inc cx
+				dec bh
+				jnz flameDarkCenterCols2
+			
+			inc dx
+			dec di
+			jnz flameDarkCenterRows2
+		pop cx
+		pop bx
+		pop dx
+		mov di, 7
+	centerRows:
+			sub cl, bl
+			mov bh, bl
+			push bx
+			sub bl, 4
+
+	currCenterCol:
+			cmp bh, bl
+			jne carryOn2
+			mov al, byte [bp]	
+			
+		carryOn2:
+			int 10h
+			inc cx
+			dec bh
+			jnz currCenterCol
+		
+		pop bx
+		
+		flameCenterRows:						;printinh flames where our column ends for asteroid
+			shr bh, 1
+			mov al, byte [bp + 4]
+			
+			flameCenterCols:
+				int 10h
+				inc cx
+				dec bh
+				jnz flameCenterCols
+			
+			push bx
+			
+			; pop di
+			; push di
+			; shr di, 1
+			; flameDarkCenterRows2:
+				; sub cl, bl
+				; sub	bl, 2
+				; mov bh, bl
+			; flameDarkCenterCols2:
+				; int 10h
+				; inc cx
+				; dec bh
+				; jnz flameDarkCenterCols2
+			
+			; inc dx
+			; dec di
+			; jnz flameDarkCenterRows2
+			
+			
+			; pop di
+			; pop dx
+			pop bx
+			; mov al, byte [bp + 6]
+			; int 10h
+			mov bh, bl
+			shr bh, 1
+			sub cl, bh
+			
+		mov al, byte [bp + 2]	
+		inc dx
+		dec di;
+		jnz centerRows
+
+		mov di, 4
+	endRows:
+			sub cl, bl
+			inc cx
+			sub bl, 2
+			mov bh, bl
+			push bx
+			sub bl, 4
+		
+		
+	currEndCol:
+			cmp bh, bl
+			jne carryOn3
+			mov al, byte [bp]
+			
+		carryOn3:
+			int 10h
+			inc cx
+			dec bh
+			jnz currEndCol
+		
+		pop bx
+		flameEndRows:						;printinh flames where our column ends for asteroid
+			shr bh, 1
+			mov al, byte [bp + 4]
+			
+			flameEndCols:
+				int 10h
+				inc cx
+				dec bh
+				jnz flameEndCols
+		
+		push bx
+		mov al, byte [bp + 6]
+		mov bx, 0x0202
+			flameDarkEndCols:
+				int 10h
+				inc cx
+				dec bh
+				jnz flameDarkEndCols
+			sub cl, bl
+			pop bx
+			
+			mov bh, bl
+			shr bh, 1
+			sub cl, bh
+			
+			; mov al, byte [bp + 6]
+			; int 10h
+		
+		mov al, byte [bp + 2]
+		inc dx
+		dec di;
+		jnz endRows
+			
+	pop ax
+	pop ax
+	pop ax
+	pop ax
+	
 	astEnd:
+	pop di
 	pop bx
 	pop dx
 	pop cx
@@ -245,6 +372,7 @@ printAsteroid:
 	pop ax
 	
 	ret	4
+
 			
 printBackground:
 		push 4			            ; radius....[bp + 18]
@@ -255,97 +383,39 @@ printBackground:
 		mov cx, 15;
 		
 	printAllStars:
-			push 0xCCCC	
-			push 5			        ; random radius form 0 to 4
-			call randGen
-			push 0xCCCC		
-			push 60			        ; random starting row from 0 to 59
-			call randGen
-			push 0xCCCC
-			push 310		        ; random starting col from 0 to 309
-			call randGen
-			call printStar
+		push 0xCCCC	
+		push 5			        ; random radius form 0 to 4
+		call randGen
+		push 0xCCCC		
+		push 60			        ; random starting row from 0 to 59
+		call randGen
+		push 0xCCCC
+		push 310		        ; random starting col from 0 to 309
+		call randGen
+		call printStar
 
-			dec cx;
-			jnz printAllStars;
+		dec cx;
+		jnz printAllStars;
+
+	printAllAsteroids:
+		push 30
+		push 40
+		call printAsteroid	
 		
-		; push 0xCCCC
-		; push 5
-		; call randGen
-		; push 35;
-		; push 70;
-		; call printStar
+		push 50
+		push 200
+		call printAsteroid
 		
-		; push 0xCCCC
-		; push 5
-		; call randGen
-		; push 10;
-		; push 80;
-		; call printStar
+		push 10
+		push 150
+		call printAsteroid
+		
+		push 17
+		push 263
+		call printAsteroid
 		
 	ret
 		
-printRect:
-	
-		mov ax, 0A000h;
-		mov es, ax;
-		mov bp, sp;
-		mov cx, [bp + 2];		    ; starting col
-		mov dx, [bp + 4];		    ; starting row
-		
-		mov bh, [bp + 10]		    ; height
-
-;--------------------------------------------------------------------
-; Printing through interrupt method
-;--------------------------------------------------------------------
-		; mov ah, 0x0C;
-		; mov al, [bp + 6]		    ; color
-			
-	; printh:
-			; mov bl, [bp + 8]		; width
-	; printrow:
-			; int 10h;
-			; add cx, 1;
-			; dec bl;
-			; jnz printrow;
-		
-		; mov cx, [bp + 2]
-		; add dx, 1;
-		; dec bh;
-		; jnz printh;
-	
-;--------------------------------------------------------------------
-; Printing through di method
-;--------------------------------------------------------------------
-	calculateloc:
-			mov ax, 320;
-			mul word dx
-			add ax, cx
-			shl ax, 1;
-			mov di, ax;
-			
-			mov ah, 0x0C;
-			mov al, [bp + 6]		    ; color
-		
-	printh:
-			mov bl, [bp + 8]		    ; width
-	printrow:
-			mov [es:di], ax;
-			add di, 1;
-			dec bl;
-			jnz printrow;
-		
-		add di, 320;
-		sub di, [bp + 8]
-		dec bh;
-		jnz printh;
-	
-; interrupt for key press
-	mov ah, 00h;
-	int 16h;
-	
-	ret 10;
-	
 ;--------------------------------------------------------------------
 ; subroutine to generate a random number
 ; generate a rand no using the system time
@@ -414,7 +484,7 @@ ClearScreen:
     mov dx, 0;		                    ; starting row
     
     mov ah, 0x0C;
-    mov al, 24		                    ; color
+    mov al, 26		                    ; color
 
     mov cx, 0
     mov dx, 66                          ; only printing 2nd section
@@ -470,7 +540,31 @@ PrintConveyorBelt:
         cmp bx, 0
 		jnz conveyor_columns;
 
-        xor bx, bx
+	conveyor_strip:
+		mov cx, word [bp + 14];		        ; starting col
+		mov dx, [bp + 12];		            ; starting row
+		add dx, 3
+		
+		mov ah, 0x0C;
+		mov al, 24					        ; color
+		mov bx, [cols]                   	; height
+
+		strip_columns:
+			mov cx, 0		                ; width
+
+			strip_rows:
+				int 10h;
+				add cx, 1;
+				cmp cx, bx
+				jnz strip_rows;
+		
+			add dx, 1;
+			cmp dx, 122
+			jnz strip_columns;
+
+    xor bx, bx
+		mov ah, 0x0C;
+		mov al, 0x12				        ; color
 
     BeltSupports:                       ; dx is already on the next row here
         
@@ -499,8 +593,12 @@ PrintConveyorBelt:
     pop bx
     pop ax 
     pop bp 
+
+	;interrupt for key press ;; is this needed here???
+	mov ah, 00h;
+	int 16h;
 	
-	ret 12      
+	ret 12
 
 PrintBox:
     push bp
@@ -524,7 +622,7 @@ PrintBox:
 			
 	box_columns:
 		mov cx, word [bp + 14]
-        mov bh, 5                   ; heigth
+        mov bh, 5                   ; no. of boxes
 
         box_rows:
             int 10h;
@@ -538,25 +636,68 @@ PrintBox:
         jnz box_rows
 
         add dx, 1
+
+		cmp dx, 95
+		jz set_color
+
         cmp dx, 115                 ; hardcoded for now??? formula can be implemented tho???
         jnz box_columns
+
+box_stars:
+	mov cx, 5
+
+	mov dx, 0D03h  ; Row=12 (in DH), Column=20 (in DL)
+	mov bh, 0      ; Page=0
+	mov ah, 02h    ; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
+	int 10h
+
+	mov bx, 000Eh  ; Page=0 (in BH), Color=14 (in BL)             14 is Yellow
+	mov ax, 0E2Ah  ; BIOS.Teletype (in AH), Character=33 (in AL)  33 is ASCII of "!"
+
+	loop_decor:
+
+		mov ah, 02h    ; BIOS.SetCursorPosition - Point to the location of dx and page no. of bh
+		int 10h
+
+		mov ax, 0E2Ah  ; BIOS.Teletype (in AH), Character=33 (in AL)  33 is ASCII of "!"
+		int 10h		; print interrupt
+
+		sub cx, 1
+		cmp cx, 2
+		jle	change_ratio
+
+		add dl, 8;
+		jnz loop_decor
+
+	change_ratio:
+		add dx, 9
+		cmp cx, 0
+		jnz loop_decor
             	
     pop dx
     pop cx
     pop bx
     pop ax 
     pop bp 
-
-    ;interrupt for key press ;; is this needed here???
-	mov ah, 00h;
-	int 16h;
 	
 	ret 12 
+
+set_color:
+	mov al, 0x29					; brown color
+	jmp box_columns
 
 
 PrintMiddle:
 
     call ClearScreen
+
+    push 0x0019			            ; height.....[bp + 20]
+	push 0x0017			            ; width......[bp + 18]
+	push 0x0042			            ; color = light brown.....[bp + 16]
+	push 13			                ; starting col.....[bp + 14]
+	push 0x005A			            ; starting row.....[bp + 12]
+
+    call PrintBox
 
     push 0x000A			            ; height.....[bp + 20]
 	push 0x0140			            ; width......[bp + 18]
@@ -566,22 +707,11 @@ PrintMiddle:
 
     call PrintConveyorBelt
 
-
-    push 0x0019			            ; height.....[bp + 20]
-	push 0x0017			            ; width......[bp + 18]
-	push 0x0043			            ; color = light brown.....[bp + 16]
-	push 13			                ; starting col.....[bp + 14]
-push 0x005A			                ; starting row.....[bp + 12]
-
-    call PrintBox
-
     ret
 
 	
 PrintMainScreen:        ; this will only call 3 funcs for now for printing sections
 
-    push 10
-	push 10
 	call printBackground
 
     call PrintMiddle
